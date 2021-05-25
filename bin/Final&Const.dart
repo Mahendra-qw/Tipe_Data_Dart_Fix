8e6bd028c3e23a7021cf52c331cb90bb09c46f89
iqbal <muhammad.mahendra@students.amikom.ac.id>
2021-05-25 22:22:00 +0700

    tambah Final&Const.dart

---
+++ bin/Final&Const.dart
@@ -0,0 +1,33 @@
+void main(){
+
+  /*
+  Final dan constanta adalah sebuah var yg bernilai tetap
+  atau tidak dapat diubah.
+  Ketika sudah mendeklarasikan var final atau const maka tidak akan bisa kita ubah
+   */
+
+  // mendeklarasikan var const
+  const phi = 3.14;
+  const double bar = 1000.55;
+  const int nilai = 22;
+  const firstname = 'Mahendra';
+
+  // phi = 22.7;
+  // nilai = 13;
+  print(phi);
+  print(bar);
+  print(nilai);
+  print("$firstname\n");
+
+  // mendeklarasikan variabel
+  final nama = "Iqbal Mahendra";
+  final double qi = 15.7;
+  final int c = 26;
+  final String Lastname = "Hendra";
+
+  // nama = 'Alfain';
+  print(nama);
+  print("$qi");
+  print("$c");
+  print("Hello my name is " + "$Lastname");
+}
